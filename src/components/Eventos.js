add09f22da30eada45f166b0059c15fcba7f9cca
NERFOO <james.jaime.1105@gmail.com>
2023-01-08 03:03:15 +0100

    chapucilla timer

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Global from '../Global';
 import axios from 'axios';
 import './css/estilos.css';
+import Timer from './Timer';
 
 export default class Eventos extends Component {
 
@@ -11,7 +12,7 @@ export default class Eventos extends Component {
     }
 
     cargarEventos = () => {
-        var request = "api/Eventos/";
+        var request = "api/TimerEventos/";
         var url = Global.url + request;
 
         axios.get(url).then( res => {
@@ -30,14 +31,24 @@ export default class Eventos extends Component {
             {
                 this.state.eventos.map((evento, index) => {
                     return (<div key={index} className="espaciadoY">
-                        <div className="card pointer" style={{width: "18rem"}}>
-                            <h3>Evento {evento.idEvento}</h3>
+                        <div className="card pointer">
+                            <h3>Evento {evento.uniqueId}</h3>
                             <div className="card-body">
-                                <h5 className="card-title">{evento.nombreEvento}</h5>
+                                <h5 className="card-title">Evento: {evento.evento}</h5>
+                                <h5 className="card-title">Empresa: {evento.empresa}</h5>
+                                <h5 className="card-title">Sala: {evento.sala}</h5>
                                 <span className="card-title">Inicio: {evento.inicioEvento}</span>
                                 <br/>
                                 <span className="card-title">Final: {evento.finEvento}</span>
                             </div>
+                        </div>
+
+
+
+
+
+                        <div>
+                            <Timer seconds={evento.duracion * 60}/>
                         </div>
                     </div>)
                 })
